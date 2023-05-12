8c06c3ac8dd396013259331aba8d656b5fa73ea2
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-05-12 00:25:50 -0300

    adicionando mais arquivos

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+
+echo " BUILD START"
+python3.9  -m pip install -r requirements.txt
+python3.9 manage.py collectstatic  --noinput --clear
+python3.9 manage.py migrate
+python3.9 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'sigprae')"
+echo " BUILD END"

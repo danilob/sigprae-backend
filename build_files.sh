e9ef4ee933dc663690aff4aa5ed179add48df387
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-05-15 09:11:25 -0300

    adicionando superusuarios

--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,5 @@ python3.9  -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py migrate
 python3.9 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'sigprae')"
+python3.9 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 echo " BUILD END"

6fb57ccd67465aa2e039180d6c72e1ba5d5a6110
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-06-06 16:58:17 -0300

    removendo espera pelo postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ $DATABASE = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# if [ $DATABASE = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-      sleep 0.1
-    done
+#     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+#       sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
+#     echo "PostgreSQL started"
+# fi
 # exclusão dos dados
 #python manage.py flush --no-input
 python manage.py makemigrations
